9bcc09300b732f35cfbdabe6430721785ff5825f
Dipta2001 <mondaldipto481@gmail.com>
2024-01-29 00:02:43 -0400

    sum

---
+++ numbers.java
@@ -0,0 +1,9 @@
+public class numbers{
+
+    public static void main(String[] args) {
+        // add two num
+        int a = 4;
+        int b = 7;
+        System.out.println("sum is : " + a + b ); 
+    }
+}
