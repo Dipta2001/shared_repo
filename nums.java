569e9f2f0da676aff62562de3db46791530b6857
Dipta2001 <mondaldipto481@gmail.com>
2024-01-29 00:24:13 -0400

    dsf

--- nums.java
+++ nums.java
@@ -2,7 +2,7 @@ public class nums{
     public static void main(String[] args) {
         int a = 4;
         int b = 6;
-        System.out.println(a+b);
+        System.out.println(a*b);
         
     }
 }
